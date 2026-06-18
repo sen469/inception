0f0f859494c385ccbc5182f3cdbd05f08c534be9
ssawa <sw20050719@gmail.com>
2026-06-18 11:19:29 +0900

    502

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -7,31 +7,46 @@ if [ ! -d "/run/mysqld" ]; then
     chown -R mysql:mysql /run/mysqld
 fi
 
+db_initialized=true
+
 # データベースが未初期化の場合のみ初期化を実行
 if [ ! -d "/var/lib/mysql/mysql" ]; then
+    db_initialized=false
     echo "Initializing MariaDB database..."
     mysql_install_db --user=mysql --datadir=/var/lib/mysql > /dev/null
+fi
 
-    # 一時的にMariaDBを起動（初期設定のため）
-    mysqld_safe --datadir=/var/lib/mysql &
-    # 起動を待つ
-    until mysqladmin ping >/dev/null 2>&1; do
-        echo "Waiting for MariaDB to start..."
-        sleep 1
-    done
+# 一時的にMariaDBを起動（初期設定のため）
+mysqld_safe --datadir=/var/lib/mysql &
 
-    # 初期設定SQLの実行
+# 起動を待つ
+until mysqladmin ping >/dev/null 2>&1; do
+    echo "Waiting for MariaDB to start..."
+    sleep 1
+done
+
+# 初期設定SQLの実行
+if [ "${db_initialized}" = false ]; then
     mysql -u root << EOF
 ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
 CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;
-CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
-GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%';
+CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+ALTER USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';
 FLUSH PRIVILEGES;
 EOF
-
-    # 一時起動したMariaDBを停止
-    mysqladmin -u root -p${MYSQL_ROOT_PASSWORD} shutdown
+else
+    mysql -u root -p"${MYSQL_ROOT_PASSWORD}" << EOF
+CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;
+CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+ALTER USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';
+FLUSH PRIVILEGES;
+EOF
 fi
+
+# 一時起動したMariaDBを停止
+mysqladmin -u root -p"${MYSQL_ROOT_PASSWORD}" shutdown
 
 # メインプロセスとしてMariaDBをフォアグラウンドで起動
 echo "MariaDB starting..."
